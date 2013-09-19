c7d55312805c100fa003494410b10c24b80af695
OpenShift guest <lujan0008@gmail.com>
2013-09-19 04:03:28 -0400

    Initialize repository”

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Calendar::Application.config.secret_key_base = '4ec2bdd1c430c6e915eae4cccd1c1e04d44efd937e6062973bb47d74b2ba799636cf0f5b0dd9adb9540c0906cb29e17311baf4e4443e8d309e26126bea483bbc'
